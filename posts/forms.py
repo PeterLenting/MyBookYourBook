a90060c1236097be450e0b77df65266ecbdfa3ba
PeterLenting <pplenting@gmail.com>
2020-01-03 10:48:39 +0000

    added basic form to add a book and basic css

--- posts/forms.py
+++ posts/forms.py
@@ -6,4 +6,6 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'content', 'image', 'tag', 'published_date')
+        fields = ('title', 'author', 'year_of_edition', 'condition_of_book',
+                  'publisher', 'number_of_pages', 'provider', 'location',
+                  'price', 'image', 'summary')
--- posts/models.py
+++ posts/models.py
@@ -5,13 +5,22 @@ from django.utils import timezone
 class Post(models.Model):
     """
     A single post
+    Title, author, year of print, condition of book, provider, location,
+    publisher, number of pages, sell, rent, price, summary
     """
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    author = models.CharField(max_length=200)
+    year_of_edition = models.CharField(max_length=4)
+    condition_of_book = models.CharField(max_length=200)
+    provider = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=200)
+    number_of_pages = models.CharField(max_length=4)
+    price = models.CharField(max_length=200)
+    summary = models.TextField(max_length=300, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
     views = models.IntegerField(default=0)
-    tag = models.CharField(max_length=30, blank=True, null=True)
     image = models.ImageField(upload_to="img", blank=True, null=True)
 
     def __unicode__(self):
