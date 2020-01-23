d0f0d62e61478e2a2fef97811c0545edd78595d2
PeterLenting <pplenting@gmail.com>
2020-01-23 14:40:38 +0000

    search app works with author, title, condition, location

--- django_mybookyourbook/urls.py
+++ django_mybookyourbook/urls.py
@@ -18,12 +18,14 @@ from django.views.generic import RedirectView
 from django.views.static import serve
 from .settings import MEDIA_ROOT
 from accounts import urls as accounts_urls
+from search import urls as urls_search
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
     url(r'^$', RedirectView.as_view(url='posts/')),
     url(r'^posts/', include('posts.urls')),
+    url(r'^search/', include(urls_search)),
     url(r'^accounts/', include(accounts_urls)),
 ]
 """
