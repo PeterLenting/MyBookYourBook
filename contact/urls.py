2141fbedc029fbf3869cb77d8ba2d05254e27b38
PeterLenting <pplenting@gmail.com>
2020-02-21 08:31:17 +0000

    deleted succes-view

--- contact/urls.py
+++ contact/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import url
-from .views import emailView, successView
 
 urlpatterns = [
     url(r'^$', emailView, name='contact'),
-    url(r'^success/', successView, name='success'),
 ]
