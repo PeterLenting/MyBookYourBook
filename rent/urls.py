b455789fd8905487bdf768dfaf38f4b8a3592622
PeterLenting <pplenting@gmail.com>
2020-03-20 12:20:01 +0000

    added rent_success.html

--- rent/urls.py
+++ rent/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import rent_view
+from .views import rent_view, rent_success_view
 
 urlpatterns = [
     url(r'^$', rent_view, name='rent'),
+    url(r'^rent_success/', rent_success_view, name='rent_success'),
 ]
--- rent/views.py
+++ rent/views.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from .forms import RentRequestForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from products.models import Product
+
+
+def rent_success_view(request):
+    """Return the rent_success.html file"""
+    return render(request,  'rent_success.html')
 
 
 @login_required()
@@ -21,15 +26,13 @@ def rent_view(request):
             'email': user.email,
             'total_amount': int(total),
             'books': y}
-    #form = RentRequestForm(initial=data)
     if request.method == "POST":
         form = RentRequestForm(request.POST)
         if form.is_valid():
-            # print("Test2")
             form.save()
             request.session['cart'] = {}
+            return redirect('rent_succes')
         else:
-            # print("Test3")
             print(form.errors)
             messages.error(request,
                            "We were unable to recieve your order")
